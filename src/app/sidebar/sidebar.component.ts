5194053e980897f7490edef2ecdbae72e8943087
Badryt <olanrewaju.badru@gmail.com>
2024-03-15 07:43:57 +0100

    fix: currentRoute fixed

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
-  currentRoute: string;
+export class SidebarComponent implements OnInit {
+  currentRoute: string ='';
   showSidebar: boolean = false;
 
-  constructor(private router: Router) {
-    this.currentRoute = this.router.url;
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.currentRoute = this.router.url; 
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
   }
 
   toggleSidebar() {
